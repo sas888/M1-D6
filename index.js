41a9c94edd2865f75a95efbaad9ad1c88169766a
sas888 <syli888@outlook.com>
2022-07-04 14:35:01 +0100

    Variables exercises

---
+++ index.js
@@ -0,0 +1,28 @@
+// To make practice with variables you should create what follows:
+
+//     a variable for your name
+//     a variable for your last name
+//     if you have a middle name, create a variable for it
+// a variable that will sum the first two variables into your full name, make sure there is a space in between name, middle name and last name.
+
+//     print on the console: hello, my name is [NAME HERE]. Make sure to use the variables!
+//     Save your age to a variable
+//     Save the current year to a variable
+//     Starting from this two information, print your birth year on the terminal, saving it to a variable.
+//     If you are uncomfortable sharing this info, you can come up with fake ones.
+// Use NodeJS in the terminal to see the outcome in the console
+// BONUS: try to run the code in the browser as well. Add a <script> tag at the end of an html <body> and check the browser’s console for the result.
+alert("Welcome!");
+
+let firstName = "John";
+let middleName = "Bob";
+let lastName = "Doe";
+
+let fullName = firstName + " " + middleName + " " + lastName;
+console.log("Hello!, My name is:", fullName);
+
+let age = 30;
+let currentYear = 2022;
+let birthYear = currentYear - age;
+
+console.log("My birth year is:", birthYear);
